4d48fd57c1fac4e0a54a42dae3f8f60d0fbf816d
Mikko Marttila <mikmart@mikkomarttila.com>
2018-06-02 10:33:39 +0300

    Add test for identifying vectors of PINs

--- tests/testthat/test-is_probably_pin.R
+++ tests/testthat/test-is_probably_pin.R
@@ -1,6 +1,10 @@
 context("test-is_probably_pin.R")
 
 test_that("valid pins are identified", {
-  # test pin from: http://vrk.fi/en/personal-identity-code1
+  # test pins from public sources:
+  #   http://vrk.fi/en/personal-identity-code1
+  #   https://en.wikipedia.org/wiki/National_identification_number#Finland
+  x <- c("311280-888Y", "311280-888Y", "131052-308T")
   expect_true(is_probably_pin("131052-308T"))
+  expect_true(is_probably_pin(x))
 })
