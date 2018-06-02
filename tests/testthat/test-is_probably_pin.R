eb3879bd8317047b0764ad7a1c2028ea72a96887
Mikko Marttila <mikmart@mikkomarttila.com>
2018-06-02 10:54:10 +0300

    Simplify test in PIN check

--- tests/testthat/test-is_probably_pin.R
+++ tests/testthat/test-is_probably_pin.R
@@ -4,9 +4,8 @@ test_that("valid pins are identified", {
   # test pins from public sources:
   #   http://vrk.fi/en/personal-identity-code1
   #   https://en.wikipedia.org/wiki/National_identification_number#Finland
-  x <- c("311280-888Y", "311280-888Y", "131052-308T")
   expect_true(is_probably_pin("131052-308T"))
-  expect_true(is_probably_pin(x))
+  expect_true(is_probably_pin(c("311280-888Y", "131052-308T")))
 })
 
 test_that("factors can be identified", {
