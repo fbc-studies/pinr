0480308ba2c3bfc06340d442f4db48a2144f163b
Mikko Marttila <mikmart@mikkomarttila.com>
2018-07-26 14:19:13 +0300

    Test recreating PINs

--- tests/testthat/test-pin.R
+++ tests/testthat/test-pin.R
@@ -10,3 +10,8 @@ test_that("pin() creates valid PINs", {
   expect_error(pin(as.Date("2152-10-13"), 308), "Invalid")
   expect_error(pin(as.Date("1952-10-13"), 3080), "Invalid")
 })
+
+test_that("pin() can recreate a PIN", {
+  pin <- c("311280-888Y", "131052-308T")
+  expect_equal(pin(pin_dob(pin), pin_ordinal(pin)), pin)
+})
